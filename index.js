96a6dfa90253ab99e7255aee692395baf3bb1384
Sergio Canto Sabido <sergio@hanullimparis.com>
2021-09-07 07:28:46 +0200

    mise en place de l'appel réseaux pour produits

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-class Camera {
-  constructor(lens, id, name, price, description, imageUrl) {
-    this.lens = lens;
-    this.id = id;
-    this.name = name
-    this.price = price;
-    this.description = description;
-    this.imageUrl = imageUrl;
-  }
-}
 
-let firstCamera = new Camera 
+let contain = document.getElementById("camera-container")
 
-
-
-function card() {
+function getAllCameras() {
     fetch("http://localhost:3000/api/cameras")
     .then(function(res) {
       if (res.ok) {
         return res.json();
       }
     })
-    .then(function(res) {
-      document
-          .getElementById("card-title")
-          .innerText = res.name;
+    .then(function(value) {
+      let nbOfValue = value.length
+      console.log(nbOfValue)
+      for(let i = 0; i < nbOfValue; i++) {
+        document
+        .getElementById("camera-container")
+        .innerHTML += `<div class='card' style='width: 18rem;'>
+                        <img src='${value[i].imageUrl}' class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">${value[i].name}</h5>
+                          <p class="card-text">${value[i].description}</p>
+                          <p>${value[i].price} EUR</p>
+                          <a href="#" class="btn btn-primary">Choisir</a>
+                        </div>
+                      </div>`
+      }
+      contain.appendChild()
+      console.log(value)
     })
     .catch(function(err) {
     // Une erreur est survenue
-    });
-  }
+    })
+}
