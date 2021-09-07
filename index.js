bcd1aadbeb992f6ea4a5893574e0b3c713a420d4
Sergio Canto Sabido <sergio@hanullimparis.com>
2021-09-07 20:23:59 +0200

    index.html mis en place

--- index.js
+++ index.js
@@ -14,14 +14,16 @@ function getAllCameras() {
       for(let i = 0; i < nbOfValue; i++) {
         document
         .getElementById("camera-container")
-        .innerHTML += `<div class='card' style='width: 18rem;'>
+        .innerHTML += `<div >
+        <div class='card' style='width: 18rem;'>
                         <img src='${value[i].imageUrl}' class="card-img-top" alt="...">
                         <div class="card-body">
-                          <h5 class="card-title">${value[i].name}</h5>
+                          <h3 class="card-title">${value[i].name}</h3>
                           <p class="card-text">${value[i].description}</p>
                           <p>${value[i].price} EUR</p>
-                          <a href="#" class="btn btn-primary">Choisir</a>
+                          <a href="product.html" class="btn btn-primary">Choisir</a>
                         </div>
+                      </div>
                       </div>`
       }
       contain.appendChild()
