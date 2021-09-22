6dc79d0fabb88204df36e7697c03ad9b76dc568a
Sergio Canto Sabido <sergio@hanullimparis.com>
2021-09-22 21:44:27 +0200

    session intense

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-getAllCameras()
-getProduit()
+//getAllCameras()
+
 
 
 // Dans index.html appel reseaux et affichage des cameras
 
 let contain = document.getElementById("camera-container")
+let mainArticle = document.getElementById("main_Article")
+let urlApi = "http://localhost:3000/api/cameras";
 
 function getAllCameras() {
     fetch("http://localhost:3000/api/cameras")
@@ -17,7 +19,6 @@ function getAllCameras() {
     .then(function(value) {
       console.log(value)
       let nbOfValue = value.length
-      console.log(nbOfValue)
       for(let i = 0; i < nbOfValue; i++) {
         let relPrice = value[i].price
         document
@@ -35,7 +36,6 @@ function getAllCameras() {
                         </div>`
       }
       contain.appendChild()
-      console.log(value)
     })
 
     .catch(function(err) {
@@ -45,40 +45,48 @@ function getAllCameras() {
 
 // Debut Produit dans product.html
 
-let mainArticle = document.getElementsById("mainArticle")
+
+
+//let chosenProduct = document.getAllCameras()._id
+//console.log(chosenProduct)
+
 function getProduit() {
-    fetch("http://localhost:3000/api/cameras")
+  let queryParams = window.location.search.substr(1).split("&");
+  let paramIdIndex = queryParams.findIndex(value => value.split("=")[0] == "id");
+  let id = queryParams[paramIdIndex].split("=")[1];
+    fetch(`${urlApi}/${id}`)
     .then(function(res) {
-    //   console.log(res)
       if (res.ok) {
         return res.json();
       }
     })
     .then(function(value) {
-        let nbOfValue = value.length
-        fetch("http://localhost:3000/api/cameras" + idProduit)
-        for(let i = 0; i < nbOfValue; i++) {
-            let idProduit = value[i]._id
-            let urlProduit = "./html/product.html" + idProduit
-            fetch(urlProduit)
             document
-            .getElementsByClassName("btn")
-            .addEventListener("btn", onclick)
-            .getElementById("mainArticle")
-            .innerHTML += `<div >
+            .getElementById("main_Article")
+            .innerHTML = `<div >
                             <div class='card' style='width: 18rem;'>
-                            <img src='${value[i].imageUrl}' class="card-img-top" alt="Camera Vintage">
+                            <img src='${value.imageUrl}' class="card-img-top" alt="Camera Vintage">
                             <div class="card-body">
-                                <h3 class="card-title">${value[i].name}</h3>
-                                <p class="card-text">${value[i].description}</p>
-                                <p>${(relPrice / 100).toFixed(2)} EUR</p>
-                                <a href="./html/product.html?id=${value[i]._id}" class="btn btn-primary" id="${value[i]._id}" class="lien">Choisir</a>
+                                <h3 class="card-title">${value.name}</h3>
+                                <p class="card-text">${value.description}</p>
+                                <p>${(value.price / 100).toFixed(2)} EUR</p>
+                                <a href="./html/product.html?id=${value._id}" class="btn btn-primary" id="${value._id}" class="lien">Choisir</a>
                                 </div>
                             </div>
-                            </div>` 
-        }
-        mainArticle.appendChild()
-        console.log(value)
+                            </div>`;
+            // .innerHTML = `<div >
+            //                 <div class='card' style='width: 18rem;'>
+            //                 <img src='${value.imageUrl}' class="card-img-top" alt="Camera Vintage">
+            //                 <div class="card-body">
+            //                     <h3 class="card-title">${value.name}</h3>
+            //                     <p class="card-text">${value.description}</p>
+            //                     <p>${(relPrice / 100).toFixed(2)} EUR</p>
+            //                     <a href="./html/product.html?id=${value._id}" class="btn btn-primary" id="${value._id}" class="lien">Choisir</a>
+            //                     </div>
+            //                 </div>
+            //                 </div>`;
+        
+      //  mainArticle.appendChild()
     })
         .catch(function(err) {
         // Une erreur est survenue
